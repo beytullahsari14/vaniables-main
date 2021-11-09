6957cf25ac7849c0a1570eb26fd9c31236ebeae1
beytullahsari14 <beytullahsari14@yandex.com>
2021-11-09 16:28:55 +0300

    Initial commit

---
+++ src/vaniables/Main.java
@@ -0,0 +1,24 @@
+package vaniables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String studentName="Beytullah";
+        int studentAge=15;
+        System.out.println(studentName);
+        studentName="Beytullah";
+        System.out.println(studentAge);
+        System.out.println(studentName+"yaþ"+studentAge);
+        String studentNationalIdentity="75612345865";
+        System.out.println("TC kimlik Numarasý :"+studentNationalIdentity);
+        double studentLength=1.67;
+        System.out.println("Öðrencinin Boyu :"+studentLength);
+        boolean studentIsActive=false; // true yada false 
+        System.out.println("Öðrenci Devam :"+studentIsActive);
+	}
+	
+}
+        
+
+     
